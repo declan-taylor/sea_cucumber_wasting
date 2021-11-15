ac3c004a86cdfa1fcbec3e35d2dad4113f803b6f
declan-taylor <declan.taylor@alumni.ubc.ca>
2021-11-15 10:08:10 -0800

    More plot modifications

--- tempData.R
+++ tempData.R
@@ -1,5 +1,6 @@
 library(here)
 library(lubridate)
+library(grid)
 library(tidyverse)
 
 # Import the data and format the date/time information
@@ -17,12 +18,46 @@ DailyLog <- read_csv("data/DailyLog.csv", col_names = TRUE) %>%
 Temp_Time <- DailyLog[35:1062,] %>%
   select(dateTime, Sea_Table, table_position, Bucket_ID, Cuke_ID, Temp_C)
 
+# TEMPERATURE PLOT
+# Create the colour gradient for the background:
+#grad <- colorRampPalette(c("#CC0000", "#0000CC"), alpha = 1)(20)
+
+TempPlot <- ggplot(data = Temp_Time,
+                   aes(x = dateTime,
+                       y = Temp_C,
+                       fill = Bucket_ID)) +
+  #annotation_custom(rasterGrob(grad, 
+  #                             width=unit(1,"npc"), 
+  #                             height=unit(1,"npc"))) +
+  geom_line() +
+  geom_hline(aes(yintercept = 22,
+             colour = "22ºC")) +
+  geom_hline(aes(yintercept = 17,
+             colour = "17ºC")) +
+  geom_hline(aes(yintercept = 12,
+             colour = "12ºC")) +
+  labs(x = "Date",
+       y = "Temperature (ºC)",
+       colour = "Treatment Temperature") +
+  scale_colour_manual(values = c("#0000CC", "#660066", "#CC0000")) +
+    guides(colour = guide_legend(reverse = TRUE)) +
+  theme_classic()
+
+TempPlot
+
+
 TempPlot <- ggplot(data = Temp_Time,
                    aes(x = dateTime,
                        y = Temp_C,
                        fill = Bucket_ID)) +
   geom_line() +
+  geom_hline(aes(yintercept = 22,
+                 colour = "#CC0000")) +
+  geom_hline(aes(yintercept = 17,
+                 colour = "#660066")) +
+  geom_hline(aes(yintercept = 12,
+                 colour = "#0000CC")) +
   labs(x = "Date",
-       y = "Temperature (ºC)")
-
-TempPlot
+       y = "Temperature (ºC)",
+       colour = "Treatment Temperature") +
+  theme_classic()
