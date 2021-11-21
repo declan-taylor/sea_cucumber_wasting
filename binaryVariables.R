b5f30c52be306d74ca63f845b1b3531345b481d2
declan-taylor <declan.taylor@alumni.ubc.ca>
2021-11-21 10:10:41 -0800

    modified binaryVariables.R to run as a function.

--- binaryVariables.R
+++ binaryVariables.R
@@ -15,95 +15,112 @@ DailyLog <- read_csv("data/DailyLog.csv", col_names = TRUE) %>%
   mutate(Sea_Table = as.factor(Sea_Table),
          Bucket_ID = as.factor(Bucket_ID))
 
-# Generate data frame to hold just the binary variables. Upper limit of 
-# dataframe is intentionally too big (I'm just removing the top 35 rows).
-SelectedData <- DailyLog %>%
-  # remove the first 34 lines of data frame (pre-experiment data).
-  tail(-34) %>%
-  # Select and rename variables
-  select(date = Date,
-         date_time = dateTime,
-         sea_table = Sea_Table,
-         table_position,
-         bucketID = Bucket_ID,
-         cukeID = Cuke_ID,
-         treatment = Treatment,
-         temp_C = Temp_C,
-         alive = Alive,
-         death_time = `Time of Death`,
-         poop = Poop,
-         evisceration = Evisceration,
-         resp_evisc = respiratory_evisceration,
-         spawn = Spawn) %>%
-  mutate(combinedID = paste(bucketID, cukeID))
+# This function generates our individual level data with death_time, spawning, 
+# evisceration, and poop data. The function exists so the dataframe can be 
+# easily made in one click.
+create_individualData <- function(master_dataframe){
+  # Generate data frame to hold just the binary variables. Upper limit of 
+  # dataframe is intentionally too big (I'm just removing the top 35 rows).
+  SelectedData <- master_dataframe %>%
+    # remove the first 34 lines of data frame (pre-experiment data).
+    tail(-34) %>%
+    # Select and rename variables
+    select(date = Date,
+           date_time = dateTime,
+           sea_table = Sea_Table,
+           table_position,
+           bucketID = Bucket_ID,
+           cukeID = Cuke_ID,
+           treatment = Treatment,
+           temp_C = Temp_C,
+           alive = Alive,
+           death_time = `Time of Death`,
+           poop = Poop,
+           evisceration = Evisceration,
+           resp_evisc = respiratory_evisceration,
+           spawn = Spawn) %>%
+    mutate(combinedID = paste(bucketID, cukeID))
+  
+  # For each individual-level variable, create a dataframe that selects only
+  # the rows for which that variable's column has data. Each dataframe is named
+  # after the variable it represents.
+  # Format that data to change all the values to "1". Select only the 
+  # combinedID and <variable> columns, and keep only the distinct combinedID
+  # values. This effectivley generates a list of buckets in which the <varible>
+  # process (i.e. pooping) occured.
+  death_time <- SelectedData %>%
+    # Filter for rows with death data
+    filter(FALSE == is.na(alive) | FALSE == is.na(death_time)) %>%
+    # Use POSIXct standard for death_time
+    mutate(death_time = ymd_hms(paste(date, death_time))) %>%
+    select(death_time, combinedID)
+  
+  evisceration <- SelectedData %>%
+    filter(FALSE == is.na(evisceration)) %>%
+    # sub text entries like "yes" for `1`.
+    mutate(evisceration = gsub("[A-z]{3}", 1, evisceration))%>%
+    select(combinedID, evisceration) %>%
+    distinct(combinedID, .keep_all = TRUE)
+  
+  resp_evisc <- SelectedData %>%
+    filter(FALSE == is.na(resp_evisc)) %>%
+    mutate(resp_evisc = gsub("[A-z]{3}", 1, resp_evisc))%>%
+    select(combinedID, resp_evisc) %>%
+    distinct(combinedID, .keep_all = TRUE)
+  
+  poop <- SelectedData %>%
+    filter(FALSE == is.na(poop)) %>%
+    mutate(poop = gsub("[A-z]{3}", 1, poop)) %>%
+    select(combinedID, poop) %>%
+    distinct(combinedID, .keep_all = TRUE)
+  
+  spawn <- SelectedData %>%
+    # text entries may be "yes" or "eggs".
+    mutate(spawn = gsub("[A-z]{3,4}", 1, spawn, ignore.case = TRUE)) %>%
+    select(combinedID, spawn) %>%
+    distinct(combinedID, .keep_all = TRUE)
+  
+  # List the variables for which dataframes were created.
+  SelectedVariables <- c("death_time", "evisceration", "resp_evisc", "poop", "spawn")
+  
+  # Generate a 1-column data frame which is a list of all the combinedID values
+  # (i.e. all the unique bins in our experiment).
+  IndividualData <- SelectedData %>%
+    select(date_time,
+           sea_table,
+           table_position,
+           bucketID,
+           cukeID,
+           combinedID,
+           treatment) %>%
+    distinct(combinedID, .keep_all = TRUE) %>%
+    drop_na()
+  
+  # Join each variable's data to the Binary
+  for(i in SelectedVariables) {
+    variable <- get(i)
+    IndividualData <- full_join(IndividualData, variable, by = "combinedID")
+    
+    IndividualData[,i] <- IndividualData[,i] %>%
+      replace_na()
+  }
 
-DeathData <- SelectedData %>%
-  # Filter for rows with death data
-  filter(FALSE == is.na(alive) | FALSE == is.na(death_time)) %>%
-  # Use POSIXct standard for death_time
-  mutate(death_time = ymd_hms(paste(date, death_time))) %>%
-  select(death_time, combinedID)
-
-EviscData <- SelectedData %>%
-  # Filter for rows with evisceration data
-  filter(FALSE == is.na(evisceration)) %>%
-  mutate(evisceration = gsub("[A-z]{3}", 1, evisceration))%>%
-  select(combinedID, evisceration) %>%
-  distinct(combinedID, .keep_all = TRUE)
-
-Resp_EviscData <- SelectedData %>%
-  # Filter for rows with evisceration data
-  filter(FALSE == is.na(resp_evisc)) %>%
-  mutate(resp_evisc = gsub("[A-z]{3}", 1, resp_evisc))%>%
-  select(combinedID, resp_evisc) %>%
-  distinct(combinedID, .keep_all = TRUE)
-
-PoopData <- SelectedData %>%
-  # Filter for rows with evisceration data
-  filter(FALSE == is.na(poop)) %>%
-  mutate(poop = gsub("[A-z]{3}", 1, poop)) %>%
-  select(combinedID, poop) %>%
-  distinct(combinedID, .keep_all = TRUE)
-
-SpawnData <- SelectedData %>%
-  mutate(spawn = gsub("[A-z]{3,4}", 1, spawn, ignore.case = TRUE)) %>%
-  select(combinedID, spawn) %>%
-  distinct(combinedID, .keep_all = TRUE)
-
-BinaryVariables <- c("DeathData", "EviscData", "Resp_EviscData", "PoopData", "SpawnData")
-BinaryData <- SelectedData %>%
-  distinct(combinedID)
-
-for(i in BinaryVariables) {
-  variable <- get(i)
-  BinaryData <- full_join(BinaryData, variable, by = "combinedID")
+  IndividualData <<- IndividualData  
 }
+  
+  IndividualData$death_time <- IndividualData$death_time %>% replace_na("0")
+  IndividualData$evisceration <- IndividualData$evisceration %>% 
+    replace_na(0) %>%
+    as.numeric(IndividualData$evisceration)
+  IndividualData$resp_evisc <- IndividualData$resp_evisc %>% 
+    replace_na(0) %>%
+    as.numeric(IndividualData$resp_evisc)
+  IndividualData$poop <- IndividualData$poop %>% 
+    replace_na(0) %>%
+    as.numeric(IndividualData$poop)
+  IndividualData$spawn <- IndividualData$spawn %>% 
+    replace_na(0) %>%
+    as.numeric(IndividualData$spawn)
 
-rm(variable, SpawnData, PoopData, EviscData)
 
-FinalBinary <- SelectedData %>%
-  select(date_time,
-         sea_table,
-         table_position,
-         bucketID,
-         cukeID,
-         combinedID,
-         treatment) %>%
-  distinct(combinedID, .keep_all = TRUE) %>%
-  drop_na()
-
-FinalBinary <- left_join(FinalBinary, BinaryData, by = "combinedID")
-
-FinalBinary$death_time <- FinalBinary$death_time %>% replace_na("0")
-FinalBinary$evisceration <- FinalBinary$evisceration %>% 
-  replace_na(0) %>%
-  as.numeric(FinalBinary$evisceration)
-FinalBinary$resp_evisc <- FinalBinary$resp_evisc %>% 
-  replace_na(0) %>%
-  as.numeric(FinalBinary$resp_evisc)
-FinalBinary$poop <- FinalBinary$poop %>% 
-  replace_na(0) %>%
-  as.numeric(FinalBinary$poop)
-FinalBinary$spawn <- FinalBinary$spawn %>% 
-  replace_na(0) %>%
-  as.numeric(FinalBinary$spawn)
+create_individualData(DailyLog)
