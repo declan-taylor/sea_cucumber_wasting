91460ef12cf723f9596bb7a2f098dfda04181eed
declan-taylor <declan.taylor@alumni.ubc.ca>
2021-11-24 11:50:07 -0800

    changed binaryVariables.R to include updated CSVs. Currently, formatting of time data is failing to parse.

--- binaryVariables.R
+++ binaryVariables.R
@@ -180,6 +180,6 @@ add_weightData <- function(datafile){
 
 # Run all 3 functions once, sequentially. Returned data frame should be 16 
 # variables across.
-create_individualData("DailyLog.csv")
-add_stressData("BehaviourData.csv")
+create_individualData("DailyLog_final.csv")
+add_stressData("BehaviourData_final.csv")
 add_weightData("SizeData.csv")
