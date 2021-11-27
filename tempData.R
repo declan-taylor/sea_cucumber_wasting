e5060225deb24ee33ed1f4ecdbcc0ce26534144e
declan-taylor <declan.taylor@alumni.ubc.ca>
2021-11-26 20:48:09 -0800

    added lots of data manipulation in tempData

--- tempData.R
+++ tempData.R
@@ -3,6 +3,8 @@ library(lubridate)
 library(grid)
 library(tidyverse)
 
+
+
 # Import the data and format the date/time information
 DailyLog <- read_csv("data/DailyLog.csv", col_names = TRUE) %>%
   # Format `Date` column to POSIX standard
@@ -13,23 +15,61 @@ DailyLog <- read_csv("data/DailyLog.csv", col_names = TRUE) %>%
   mutate(dateTime = ymd_hms(dateTime)) %>%
   # Make `Sea_Table` and `Bucket_ID` factorial data.
   mutate(Sea_Table = as.factor(Sea_Table),
-         Bucket_ID = as.factor(Bucket_ID))
+         Bucket_ID = as.factor(Bucket_ID)) %>%
+  select(date = Date,
+         date_time = dateTime,
+         sea_table = Sea_Table,
+         table_position,
+         bucketID = Bucket_ID,
+         cukeID = Cuke_ID,
+         treatment = Treatment,
+         temp_C = Temp_C,
+         alive = Alive,
+         death_time = `Time of Death`,
+         poop = Poop,
+         evisceration = Evisceration,
+         resp_evisc = respiratory_evisceration,
+         spawn = Spawn) %>%
+  mutate(tableID = paste(sea_table, table_position)) %>%
+  tail(-34)
 
-Temp_Time <- DailyLog[35:1062,] %>%
-  select(dateTime, Sea_Table, table_position, Bucket_ID, Cuke_ID, Temp_C)
+# Making a new dataframe to get some values about the range and varition in our
+# temperature data.
+tempRange <- DailyLog %>%
+  filter(date == "2021-11-11" | date == "2021-11-12") %>%
+  select(date, date_time, tableID, treatment, temp_C) %>%
+  filter(treatment == "control")
+
+range(tempRange$temp_C)
+mean(tempRange$temp_C)
+# FOR CONTROL: 11.2 to 14.0, mean = 12.5
+# FOR ROOM: 14.9 to 17.9, mean = 16.79
+# FOR HEAT: 20.0 to 23.3, mean =  21.86
+
+
+
+death_time <- DailyLog %>%
+  # Filter for rows with death data
+  filter(FALSE == is.na(alive) | FALSE == is.na(death_time)) %>%
+  # Use POSIXct standard for death_time
+  mutate(death_time = ymd_hms(paste(date, death_time))) %>%
+  select(death_time, bucketID, temp_C)
+
+Temp_Time <- DailyLog %>%
+  select(date_time, sea_table, table_position, bucketID, tableID, temp_C)
 
 # TEMPERATURE PLOT
 # Create the colour gradient for the background:
 #grad <- colorRampPalette(c("#CC0000", "#0000CC"), alpha = 1)(20)
 
-TempPlot <- ggplot(data = Temp_Time,
-                   aes(x = dateTime,
-                       y = Temp_C,
-                       fill = Bucket_ID)) +
-  #annotation_custom(rasterGrob(grad, 
-  #                             width=unit(1,"npc"), 
-  #                             height=unit(1,"npc"))) +
-  geom_line() +
+TempPlot <- ggplot() +
+  geom_line(data = Temp_Time,
+            aes(x = date_time,
+                y = temp_C,
+                fill = bucketID)) +
+  geom_point(data = death_time,
+             aes(x = death_time,
+                 y = temp_C)) +
   geom_hline(aes(yintercept = 22,
              colour = "22ºC")) +
   geom_hline(aes(yintercept = 17,
