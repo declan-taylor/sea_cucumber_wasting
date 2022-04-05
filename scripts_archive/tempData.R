7398780b565bb0481db56cf6d65ea8c80ee4d463
declan-taylor <declan.taylor@alumni.ubc.ca>
2022-04-04 21:53:05 -0700

    Created the 'frequencey plot' temperature figure.

--- scripts_archive/tempData.R
+++ scripts_archive/tempData.R
@@ -118,7 +118,7 @@ TempPlot <- ggplot() +
        y = "Temperature (ºC)",
        colour = "Treatment Temperature") +
   scale_colour_manual(values = c("#0000CC", "#660066", "#CC0000")) +
-    guides(colour = guide_legend(reverse = TRUE)) +
+  guides(colour = guide_legend(reverse = TRUE)) +
   scale_y_continuous(breaks = c(12, 14, 16, 18, 20, 22, 24))+
   theme_classic()
 
@@ -140,7 +140,7 @@ factored_temp <- Temp_Time %>%
             max = max(temp_C)) %>%
   drop_na()
 
-ggplot() +
+AverageTemps <- ggplot() +
   geom_ribbon(data = factored_temp,
               aes(x = date_time,
                   ymin = min,
@@ -157,11 +157,68 @@ ggplot() +
        y = "Temperature (ºC)",
        colour = "Treatment") +
   scale_colour_manual(values = c("#0000CC", "#660066", "#CC0000")) +
-  guides(colour = guide_legend(reverse = TRUE)) +
-  scale_y_continuous(breaks = c(12, 14, 16, 18, 20, 22, 24))+
+  #scale_fill_gradient2(low = "dodgerblue1",
+  #                      mid = "lightyellow2",
+  #                      high = "orangered1",
+  #                      midpoint = 12,
+  #                     guide = "colourbar",
+  #                     space = "Lab") +
   theme_classic()
 
+AverageTemps
 
+# I'm also going to create a figure which is a subsetted version of the above,
+# which just includes the range of the temperature within each treatment, and
+# stacks these as bars to indicate the frequency of each temperature in each
+# treatment.
+factored_temp_3day <- Temp_Time %>%
+  filter(date == "2021-11-10" | date == "2021-11-11" | date == "2021-11-12") %>%
+  dplyr::select(treatment, temp_C) %>%
+  mutate("label" = "Treatment temperature")
 
+average_temp <- factored_temp_3day %>%
+  group_by(treatment) %>%
+  summarise(average = mean(temp_C),
+            min = min(temp_C),
+            max = max(temp_C))
+  
 
+AverageTemps_3day <- 
+  ggplot(data = factored_temp_3day,
+         aes(x = label, y = temp_C, colour = temp_C)) + 
+    geom_hline(aes(yintercept = temp_C,
+                   colour = temp_C),
+               size = 2) +
+    geom_hline(data = average_temp,
+               aes(yintercept = average),
+               colour = "black",
+               size = 0.7) +
+    geom_point(colour = "black",
+               size = 0.4,
+               position = position_jitter(width = 0.55,
+                                          height = 0)) +
+    geom_hline(data = average_temp,
+               aes(yintercept = min - 0.1),
+               colour = "black",
+               linetype = "dotted") +
+    geom_hline(data = average_temp,
+               aes(yintercept = max + 0.1),
+               colour = "black",
+               linetype = "dotted") +
+    scale_color_gradient2(low = "dodgerblue1",
+                          mid = "lightyellow2",
+                          high = "orangered1",
+                          midpoint = 13) +
+    ylim(5,24) +
+    theme_classic() +
+  labs(x = "",
+       y = "Temperature (ºC)") +
+    theme(legend.position = "none") +
+  theme(aspect.ratio=33/9)
 
+AverageTemps_3day
+
+ggsave("AverageTemps_3day.pdf",
+       AverageTemps_3day,
+       device = "pdf",
+       path = here("figures"))
